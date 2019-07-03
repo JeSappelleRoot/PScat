eea9e962700f538cad634e4712f27fe90e7b6bc9
JeSappelleRoot <joseph.morin.it@gmail.com>
2019-07-04 01:14:52 +0200

    Finally push the final script...no intermediate commit

--- PScat.ps1
+++ PScat.ps1
@@ -1,6 +1,7 @@
 param(
+    # Adding argument to command line
     [string]$target,
-    [string]$port,
+    [int]$port,
     [string]$mode
 )
 
@@ -33,7 +34,7 @@ function checkTcp($ip,$port) {
 
     # Initialize a TCP socket via Net object
     $tcpSocket = New-Object Net.Sockets.TcpClient
-
+    
     # Basic try/catch
     try {$tcpSocket.Connect($target,$port)}
     catch {}
@@ -63,7 +64,7 @@ function checkUdp($ip,$port) {
     #Get bytes from string
     $bytes = $asciiEncoder.GetBytes("Knock Knock Neo...follow the white rabbit")
     #Send bytes through UDP socket
-    $udpSocket.Send($bytes, $bytes.Length)
+    $udpSocket.Send($bytes, $bytes.Length) | Out-Null
     
     # Initialize a listener to get the answer of remote target
     $remoteEndpoint = New-Object system.net.ipendpoint([system.net.ipaddress]::Any,0)
@@ -89,6 +90,53 @@ function checkUdp($ip,$port) {
     return
 }
 
+function checkArgs($arguments) {
+# Function to check command line args
+
+<#
+The parameter of the function take $PsBoundParameters which contains arguments
+The $arguments variable begin a classic dictionnary (Key and associated Value)
+#>
+    # Get value of keys with a simple parsing
+    $target = $arguments['target']
+    $port = $arguments['port']
+    $mode = $arguments['mode']
+
+    # Write somes examples in case of wrong command line
+    $example = '
+    Examples : 
+    - TCP check : PScat.ps1 -target server.company.com -mode tcp -port 80
+    - UDP check : PScat.ps1 -target server.company.com -mode udp -port 161
+    '
+
+    # If $target is empty
+    if (-not($target)) {
+        Write-Host "[!] Please specify a host destination with -target argument" -ForegroundColor Yellow
+        $example
+        Exit
+    }
+    # Else if $port is empty
+    elseif (-not($port)) {
+        Write-Host "[!] Please specify a port with -port argument" -ForegroundColor Yellow
+        $example
+        Exit
+    }
+    # Else if port is lower than 1 or greater than 65535 (min & max in network port range)
+    elseif ($port -lt 1 -or $port -gt 65535) {
+        Write-Host "[!] Please specify a port between 1 and 65535" -ForegroundColor Yellow
+        $example
+        Exit
+    }
+    # Else if the $mode variable get wrong string
+    elseif ($mode -ne 'tcp' -and $mode -ne 'udp') {
+        Write-Host "[!] Please specify mode with [udp] or [tcp]" -ForegroundColor Yellow
+        $example
+        Exit
+    }
+
+    return
+}
+
 
 # ------------------------------------------------------
 # ------------------- Begin of PScat -------------------
@@ -97,15 +145,19 @@ function checkUdp($ip,$port) {
 # Display funny banner with Freddy cat ! 
 displayBanner
 
+# Check arguments gived in command line
+checkArgs $PsBoundParameters
 
 
-$target = '192.168.8.24'
-$port = 24
-$mode = 'tcp'
+# Get value from $PSBoundParameters dictionnary 
+$target = $PSBoundParameters['target']
+$port = $PSBoundParameters['port']
+$mode = $PSBoundParameters['mode']
 
+# Switch between mode
 switch ($mode) {
-    'tcp' {checkTcp $target $port}
-    'udp' {checkUdp $target $port}
+	'tcp' {checkTcp $target $port}
+	'udp' {checkUdp $target $port}
 }
 
 
